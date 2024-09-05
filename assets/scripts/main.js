1cd5594d1eb1f31ad05148ccf17d83d9a0ce858b
StasNik <stas.nik12@gmail.com>
2024-09-05 12:30:05 +0300

    Добавил скрипт для карты

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,6 +26,28 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 
+// Карта
+// *****
+const mapTitle = document.createElement('div'); mapTitle.className = 'sch-map-title';
+const schMap = document.querySelector('.footer-sch-map');
+
+ mapTitle.textContent = 'Для активации карты нажмите по ней';
+ schMap.appendChild(mapTitle);
+
+ schMap.onclick = function() {
+     this.children[0].removeAttribute('style');
+     mapTitle.parentElement.removeChild(mapTitle);
+ }
+
+ schMap.onmousemove = function(event) {
+     mapTitle.style.display = 'block';
+     if(event.offsetY > 10) mapTitle.style.top = event.offsetY + 20 + 'px';
+     if(event.offsetX > 10) mapTitle.style.left = event.offsetX + 20 + 'px';
+ }
+ schMap.onmouseleave = function() {
+     mapTitle.style.display = 'none';
+ }
+
 
 
 // Owl-carousel
