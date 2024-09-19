131e6914b78f07d75ae2bd698093d716d914aabf
StasNik <stas.nik12@gmail.com>
2024-09-19 12:38:07 +0300

    Перенос скрипт вывод даты в index html

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,27 +1,27 @@
-// Вывод даты
-// *****
-const d = new Date();
-const day = ["Воскресенье,", "Понедельник,", "Вторник,", "Среда,", "Четверг,", "Пятница,", "Суббота,"];
-const month = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+// // Вывод даты
+// // *****
+// const d = new Date();
+// const day = ["Воскресенье,", "Понедельник,", "Вторник,", "Среда,", "Четверг,", "Пятница,", "Суббота,"];
+// const month = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
 
-// Форматирование даты для атрибута datetime
-const datetimeString = d.getFullYear() + '-' + 
-                       String(d.getMonth() + 1).padStart(2, '0') + '-' + 
-                       String(d.getDate()).padStart(2, '0') + 'T' + 
-                       String(d.getHours()).padStart(2, '0') + ':' + 
-                       String(d.getMinutes()).padStart(2, '0') + ':' + 
-                       String(d.getSeconds()).padStart(2, '0');
+// // Форматирование даты для атрибута datetime
+// const datetimeString = d.getFullYear() + '-' + 
+//                        String(d.getMonth() + 1).padStart(2, '0') + '-' + 
+//                        String(d.getDate()).padStart(2, '0') + 'T' + 
+//                        String(d.getHours()).padStart(2, '0') + ':' + 
+//                        String(d.getMinutes()).padStart(2, '0') + ':' + 
+//                        String(d.getSeconds()).padStart(2, '0');
 
-// Установка значения атрибута datetime
-document.getElementById("currentTime").setAttribute("datetime", datetimeString);
+// // Установка значения атрибута datetime
+// document.getElementById("currentTime").setAttribute("datetime", datetimeString);
 
-// Вывод текста в элемент
-const outputText = day[d.getDay()] + " " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
-document.querySelector("#currentTime span").innerHTML = outputText;
+// // Вывод текста в элемент
+// const outputText = day[d.getDay()] + " " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+// document.querySelector("#currentTime span").innerHTML = outputText;
+
+// // Вывод подсказки в дате
+// // *****
 document.querySelector(".header-top-date a").setAttribute("data-bs-title", outputText);
-
-// Вывод подсказки в дате
-// *****
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
