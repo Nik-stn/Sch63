5529bf299cc26fc3624704630bec56ed00f4dbc0
StasNik <stas.nik12@gmail.com>
2024-10-29 09:26:28 +0300

    Добавил pop up Политика конфедициальности

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -172,7 +172,63 @@ $('.owl-carousel.owl-banners-official, .owl-carousel.owl-banners').owlCarousel({
 
 // Fancy-box
 // *****
-Fancybox.bind('[data-fancybox="gallery"]', {
-    // Your custom options for a specific gallery
-  });
+const fancy = document.querySelectorAll('[data-fancybox="gallery"]');
+if (fancy.length > 0) {
+    Fancybox.bind(fancy.forEach(item => item, {
+        // Your custom options for a specific gallery
+    }));
+}
+
+
+// Privacy-policy
+// *****
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+
+    options = {
+        path: '/',
+        // при необходимости добавьте другие значения по умолчанию
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    
+
+    document.cookie = updatedCookie;
+}
+
+console.log(getCookie('privacy_policy'));
+
+if (getCookie('privacy_policy') === undefined) {
+    document.getElementById('privacy_policy').style.display = '';
+console.log(document.getElementById('privacy_policy'));
+}
+
+document.getElementById('privacy_policy_btn').addEventListener('click', function () {
+    var date = new Date;
+    date.setDate(date.getDate() + 1);
+    setCookie('privacy_policy', 1, { expires: date });
+    document.getElementById('privacy_policy').style.display = 'none';
+});
   
